4d1940df069b6c8ffcbaaf2b2059f83bd2468dc4
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-07-06 22:35:11 +0300

    Finishing Up Feed Cell UI

--- InstagramClone/InstagramClone/Views/FeedCell.swift
+++ InstagramClone/InstagramClone/Views/FeedCell.swift
@@ -11,7 +11,7 @@ class FeedCell: UICollectionViewCell {
     
     // MARK: - PROPERTIES
     
-    private lazy var profileImageView: UIImageView = {
+    private let profileImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
         iv.clipsToBounds = true
@@ -27,6 +27,60 @@ class FeedCell: UICollectionViewCell {
         button.addTarget(self, action: #selector(didTapUserName), for: .touchUpInside)
         return button
     }()
+    
+    private let posImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.isUserInteractionEnabled = true
+        iv.image = UIImage(named: "venom-7")
+        return iv
+    }()
+    
+    private lazy var likeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(imageLiteralResourceName: "like_unselected"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var commentButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(imageLiteralResourceName: "comment"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var shareButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(imageLiteralResourceName: "send2"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private let likesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "1 like"
+        label.font = UIFont.boldSystemFont(ofSize: 12)
+        return label
+    }()
+    
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Some test caption for now.."
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    private let postTimeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2 days ago"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .lightGray
+        return label
+    }()
+    
+  
     
     // MARK: - LIFECYCLE
     
@@ -47,8 +101,25 @@ class FeedCell: UICollectionViewCell {
         userNameButton.centerY(inView: profileImageView,
                                leftAnchor: profileImageView.rightAnchor,
                                paddingLeft: 8)
+        
+        addSubview(posImageView)
+        posImageView.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 8)
+        posImageView.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1).isActive = true
+        
+        configureActionButtons()
+        
+        addSubview(likesLabel)
+        likesLabel.anchor(top: likeButton.bottomAnchor, left: leftAnchor, paddingTop: -4, paddingLeft: 8)
+        
+        addSubview(captionLabel)
+        captionLabel.anchor(top: likesLabel.bottomAnchor, left: leftAnchor, paddingTop: 8, paddingLeft: 8)
+        
+        addSubview(postTimeLabel)
+        postTimeLabel.anchor(top: captionLabel.bottomAnchor, left: leftAnchor, paddingTop: 8,
+        paddingLeft: 8)
     }
     
+   
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -57,5 +128,16 @@ class FeedCell: UICollectionViewCell {
     
     @objc func didTapUserName() {
         print("DEBUG: did tap username")
+    }
+    
+    // MARK: HELPERS
+    
+    func configureActionButtons() {
+       let stackView = UIStackView(arrangedSubviews: [likeButton, commentButton, shareButton])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        
+        addSubview(stackView)
+        stackView.anchor(top: posImageView.bottomAnchor, width: 120, height: 50)
     }
 }
