eb459cb7b75033e5472c28f87707c0bf5e3dcfd7
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-07-07 14:46:08 +0300

    added Login Page UI

---
+++ InstagramClone/InstagramClone/Controllers/Authentication/RegistrationController.swift
@@ -0,0 +1,21 @@
+//
+//  RegistrationController.swift
+//  InstagramClone
+//
+//  Created by Khusrav Safiev on 7/6/24.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - PROPERTIES
+    
+    // MARK: - LIFECYCLE
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPurple
+    }
+}
